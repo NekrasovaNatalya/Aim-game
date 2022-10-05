ce833347a59aecf734d74d31e8b3996e35704927
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-10-05 19:41:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const board = document.querySelector('.board');
 const colors = ['#186950', '#a4c3dc', '#799b3e', '#ffb7c8', '#d84a19', '#face8b', '#43c188', '#c62a56', '#7b3e84', '#93b123'];
 let time = 0;
 let score = 0;
+
+startBtn.addEventListener('click', event => {
+    event.preventDefault();
+    screens[0].classList.add('up');
+});
